e385bd428f79044c52fa633ad7680e3797ad6941
Sunny Jin <sunny.jin@flex.com>
2017-10-26 23:23:56 -0700

    [Extension] Update application settings

--- server/config.js
+++ server/config.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
     // Start URL that browsing should start at.
-    startURL: 'https://www.w3schools.com http://jqueryui.com/demos/',
+    startURL: 'https://chinaltp.hkd.flextronics.com/powerhr2000.net/Loginx.aspx',
     // Start Method when there is no start URL.  This is fpr when we are doing an API integration,
     // and we don't actually need to load phantom (yet).
     // startMethod: "login.onStart",
